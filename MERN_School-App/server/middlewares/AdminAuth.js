3651bf03e0cc9e2481e4c0003c08815a58599dd0
Karandas333 <146736956+Karandas333@users.noreply.github.com>
2024-09-22 12:47:24 +0530

    Update AdminAuth.js

--- MERN_School-App/server/middlewares/AdminAuth.js
+++ MERN_School-App/server/middlewares/AdminAuth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports.AdminAuth = (req, res, next) => {
   const token = req.headers['authorization']?.split(' ')[1];
-
+  console.log(token)
   if (token) {
     try {
       const userData = jwt.verify(token, process.env.JWT_KEY);

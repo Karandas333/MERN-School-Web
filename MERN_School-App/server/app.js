bf229e080260d94181d3eab3fdcca8204028593f
Karandas333 <146736956+Karandas333@users.noreply.github.com>
2024-09-22 10:02:21 +0530

    Update app.js

--- MERN_School-App/server/app.js
+++ MERN_School-App/server/app.js
@@ -30,13 +30,6 @@ app.use(
   })
 );
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', './dist/index.html'));
-});
-
 
 app.use('/api/info',InfoRouter)
 app.use('/api/admission',AdmissionRouter)
